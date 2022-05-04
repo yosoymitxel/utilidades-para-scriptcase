0b03d59d692a1745a9ea6c8083fe37e832743ce9
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2022-05-04 10:29:00 -0400

    Update conseguir-titulos-nombre-app-de-menu.js

--- js/conseguir-titulos-nombre-app-de-menu.js
+++ js/conseguir-titulos-nombre-app-de-menu.js
@@ -1,40 +1,56 @@
-function getTituloNombreApp(){
-        let re = /=(grd|form|frm|control|ctr|ctrl|grid|menu|proc|app|Ctr|dashboard)_(\w)+/;
-        let obviar = /(#)/;
-        var tituloApp = [];
-        var nombreApp = [];
-
-        console.log($('.topmenu .toproot ul li a'))
-
-        $('.topmenu .toproot ul li a').each(function( index ) {
-            console.log( index + ": " + $( this ).text() );
-            let titulo = $( this ).text();
-            let href   = $( this ).attr('item-href');
-            console.log(href);
-            //console.info(re.exec(href));
-            if(!href.match(obviar)){
-                tituloApp.push( titulo );
-                console.log(!(re.exec(href)))
-                nombreApp.push( (re.exec(href) ? (re.exec(href)[0]).substr(1) : 'NO SE HA ENCONTRADO ENLACE: '.href) );
+function getTituloNombreApp(enumerar=true,delimitador='\n',copiarPortapapeles=true){
+        let re        = /=(grd|form|frm|control|ctr|ctrl|grid|menu|proc|app|Ctr|dashboard)_(\w)+/
+        let obviar    = /(#)/
+        var tituloApp = []
+        var enlaceApp = []
+        var listaApps = $('.topmenu .toproot ul li a')
+    
+        console.log(listaApps)
+    
+        if(listaApps){
+            //Bucle de listado de aplicaciones
+            $(listaApps).each(function( index ) {
+                /*Debug: Obtener titulo*/
+                //console.log( index + ": " + $( this ).text() );
+                
+                let titulo = $( this ).text();
+                let href   = $( this ).attr('item-href');
+                
+                /*Debug: Obtener el enlace*/
+                //console.log(href);
+    
+                //Se obvian los titulos superiores
+                if(!href.match(obviar)){
+                    tituloApp.push( titulo );
+                
+                    //Se buscan los nombres de apps en base a la nomenclatura (Ejemp: form_)
+                    enlaceApp.push( (re.exec(href) ? (re.exec(href)[0]).substr(1) : 'NO SE HA ENCONTRADO ENLACE: '.href) );
+                }
+                
+            });
+            console.log(tituloApp);
+            console.log(enlaceApp);
+    
+            //Copiar en portapapeles
+            if(copiarPortapapeles){
+                let dato = '';
+    
+                for (let i=0;i<tituloApp.length;i++){
+                    let contador = enumerar ? (i + 1)+delimitador : ''
+                    dato += `${contador}${tituloApp[i]}${delimitador}${enlaceApp[i]}\n`;
+                }
+        
+                let $temp = $("<textarea>");
+                $("body").append($temp);
+                $temp.val(dato).select();
+                document.execCommand("copy");
+                $temp.remove();
             }
-            console.info('Se guardó\n');
-        });
-        console.log(tituloApp);
-        console.log(nombreApp);
-
-        //Copiar en portapapeles
-        let dato = '';
-
-        for (let i=0;i<tituloApp.length;i++){
-            dato += `${i+1}\n${tituloApp[i]}\n${nombreApp[i]}\n`;
+            
+            return [tituloApp,enlaceApp];
         }
 
-        let $temp = $("<textarea>");
-        $("body").append($temp);
-        $temp.val(dato).select();
-        document.execCommand("copy");
-        $temp.remove();
-        return true;
+        
     }
     
     getTituloNombreApp()
