efc263c23b2ff7a202d503ee1101375cc9bde8be
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2022-05-06 08:18:57 -0400

    Listar campos

--- js/listar-campos.js
+++ js/listar-campos.js
@@ -1,6 +1,10 @@
-var vartext = ''
-$('.jstree-children a[id^=fields_tit_itens_]').each(function(index,obj){
-    vartext += obj.text+'\n'
-   
-})
-console.log(vartext)
+function us_ListarCampos(){
+    var vartext = ''
+    $('.jstree-children a[id^=fields_tit_itens_]').each(function(index,obj){
+        vartext += obj.text+'\n'
+       
+    })
+    console.log(vartext)
+}
+
+us_ListarCampos()
