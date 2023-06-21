7691ed04962a7bf92573caee4548246b9b952a22
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2023-06-21 09:19:06 -0400

    Update convertir-select-a-select2-en-selector-de-tabla.js

--- js/convertir-select-a-select2-en-selector-de-tabla.js
+++ js/convertir-select-a-select2-en-selector-de-tabla.js
@@ -6,7 +6,7 @@ Esta permite usar un buscador para hallar más facilmente la tabla que deseamos.
 
 */
 
-function selectToSelect2(ids='#sel_def_table,#id_NomeTabela'){
+function selectToSelect2(ids='#sel_def_table,#id_NomeTabela,select.nmInput'){
   $('head').append(`<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>`);
   $(ids).select2();
